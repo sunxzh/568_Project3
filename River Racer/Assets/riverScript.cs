613f2499fba96cd56ca80f5133056070b82550f5
josephto <joccerT25@gmail.com>
2014-11-18 21:58:51 -0500

    random obstacle generation on river

--- River Racer/Assets/riverScript.cs
+++ River Racer/Assets/riverScript.cs
@@ -3,9 +3,25 @@ using System.Collections;
 
 public class riverScript : MonoBehaviour {
 
+	public GameObject[] obstacles;
+	public int numObstacles;
+
 	// Use this for initialization
 	void Start () {
-	
+		Mesh riverMesh = this.gameObject.GetComponent<MeshFilter> ().mesh;
+		Vector3[] vertices = riverMesh.vertices;
+
+		for (int i = 0; i < numObstacles; i++){
+			int rndIndex = Random.Range (0,vertices.Length-1);
+			Vector3 pos1 = transform.TransformPoint(vertices[rndIndex]);
+			Vector3 pos2 = transform.TransformPoint(vertices[rndIndex+1]);
+			float rndInterp = Random.Range (0f,1f);
+			Vector3 obstaclePos = (pos2-pos1)*rndInterp + pos1;
+			int rndObstacleIdx = Random.Range (0,obstacles.Length);
+			GameObject obs = (GameObject) Instantiate (obstacles[rndObstacleIdx], obstaclePos, Quaternion.identity);
+			float rndScale = Random.Range (4f,7f);
+			obs.transform.localScale = new Vector3(rndScale, rndScale, rndScale);
+		}
 	}
 
 	void OnTriggerStay(Collider collider){
